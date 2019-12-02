af09fc32f898887a32d8aa6e7d8cd4821b1be2ab
Nexey <leliam@live.fr>
2019-12-02 20:13:50 +0100

    Initial commit

---
+++ src/VueBillardFactice.java
@@ -0,0 +1,30 @@
+
+import vues.VueBillard;
+
+public class VueBillardFactice implements VueBillard {
+
+	@Override
+	public double largeurBillard() {
+// TODO Auto-generated method stub
+		return 200;
+	}
+
+	@Override
+	public double hauteurBillard() {
+// TODO Auto-generated method stub
+		return 100;
+	}
+
+	@Override
+	public void miseAJour() {
+// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void montrer() {
+// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/vues/TestCadreAngryBallsSeul.java
@@ -0,0 +1,19 @@
+package vues;
+
+import java.awt.Frame;
+import java.util.Vector;
+
+import modele.Bille;
+
+public class TestCadreAngryBallsSeul {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Vector<Bille> billes = new Vector<Bille>();
+		CadreAngryBalls cadre = new CadreAngryBalls("angry balls", "animation de billes marrantes", billes);
+		cadre.montrer();
+	}
+
+}
